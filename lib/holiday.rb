03fe496c25179248ebf09a42fd47fd667ca9402f
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-06-02 04:15:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,10 @@ def all_winter_holiday_supplies(holiday_hash)
 
 holiday_hash[:winter].collect do |holiday, array|
   array 
-end.flatten 
+end
+.flatten 
+
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:

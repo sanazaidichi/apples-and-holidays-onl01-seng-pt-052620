ea964c41bf3d157a47f51ba960a1d166512ed686
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-06-02 04:59:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, h|
   puts "#{season.to_s.capitalize}:"
   h.each do |holiday, array|
-    str = holiday.to.split("_")
+    str = holiday.to_s.split("_")
     new_str = str.map{|w| w.capitalize }.join(" ")
     
      

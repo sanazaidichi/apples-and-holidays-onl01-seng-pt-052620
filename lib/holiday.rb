c63ea2fb8b91612dbeb75c59c7d73f13d0f3cf12
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-06-02 03:51:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,6 +29,12 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash[:winter][:christmas] << supply 
 holiday_hash[:winter][:new_years] << supply 
+
+  holiday_hash[:winter].each do |holiday, array|
+  array.push(supply)
+  
+  end 
+
 end
 
 
